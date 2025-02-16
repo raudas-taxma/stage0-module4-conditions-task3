2b91fa7199062eda78d602ba7c58aaecac00c957
rgsad <rsad3262@gmail.com>
2025-02-17 00:01:04 +0300

    solution

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int days = 0;
+        if (month < 0 || month > 12) {
+            System.out.println("wrong number!");
+            return;
+        } else if (month == 2) {
+            days = 28;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            days = 30;
+        } else {
+            days = 31;
+        }
+        System.out.println(days);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double tax = 0;
+
+        if (salary < 0) {
+            System.out.println("wrong input!");
+            return;
+        }
+        else if (salary <= 10000) {
+            tax = 0.15;
+        } else if (salary <= 20000) {
+            tax = 0.18;
+        } else {
+            tax = 0.2;
+        }
+
+        double totalSalary = salary - salary * tax;
+        System.out.println(totalSalary);
     }
 }
